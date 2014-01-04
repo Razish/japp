449cbd944f0c577d6380fc07004467515d8475fd
Razish <mrrazish@gmail.com>
2014-01-04 16:39:45 +1100

    Fix NPCs jump state not being set

--- game/g_ICARUScb.c
+++ game/g_ICARUScb.c
@@ -2581,7 +2581,7 @@ static qboolean Q3_SetBState( int entID, const char *bs_name )
 	}
 
 	bSID = (bState_t)(GetIDForString( BSTable, bs_name ));
-	if ( bSID > -1 )
+	if ( bSID != -1 )
 	{
 		if ( bSID == BS_SEARCH || bSID == BS_WANDER )
 		{
@@ -2707,7 +2707,7 @@ static qboolean Q3_SetTempBState( int entID, const char *bs_name )
 	}
 
 	bSID = (bState_t)(GetIDForString( BSTable, bs_name ));
-	if ( bSID > -1 )
+	if ( bSID != -1 )
 	{
 		ent->NPC->tempBehavior = bSID;
 	}
@@ -2757,7 +2757,7 @@ static void Q3_SetDefaultBState( int entID, const char *bs_name )
 	}
 
 	bSID = (bState_t)(GetIDForString( BSTable, bs_name ));
-	if ( bSID > -1 )
+	if ( bSID != -1 )
 	{
 		ent->NPC->defaultBehavior = bSID;
 	}
