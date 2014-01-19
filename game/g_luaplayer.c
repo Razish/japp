654e2399bcde6d0c781d4d1d08d038ebe34082e4
Razish <mrrazish@gmail.com>
2014-01-20 07:50:52 +1100

    Added JPLua Player:GetUserinfo() to server

--- game/g_luaplayer.c
+++ game/g_luaplayer.c
@@ -39,51 +39,18 @@ static int JPLua_Player_ToString( lua_State *L ) {
 	return 1;
 }
 
-//Func: Player:GetID()
-//Retn: integer of the client's ID
-static int JPLua_Player_GetID( lua_State *L ) {
+//Func: Player:GetAngles()
+//Retn: Table of pitch/yaw/roll view angles
+static int JPLua_Player_GetAngles( lua_State *L ) {
+	int top = 0;
 	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
-	lua_pushinteger( L, player->clientNum );
-	return 1;
-}
 
-//Func: Player:GetName()
-//Retn: string of the Player's name
-static int JPLua_Player_GetName( lua_State *L ) {
-	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
-	lua_pushstring( L, level.clients[player->clientNum].pers.netname );
-	return 1;
-}
+	lua_newtable( L );
+	top = lua_gettop( L );
 
-//Func: Player:GetHealth()
-//Retn: integer amount of the Player's health
-static int JPLua_Player_GetHealth( lua_State *L ) {
-	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
-	lua_pushinteger( L, level.clients[player->clientNum].ps.stats[STAT_HEALTH] );
-	return 1;
-}
-
-//Func: Player:GetArmor()
-//Retn: integer amount of the Player's armor
-static int JPLua_Player_GetArmor( lua_State *L ) {
-	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
-	lua_pushinteger( L, level.clients[player->clientNum].ps.stats[STAT_ARMOR] );
-	return 1;
-}
-
-//Func: Player:GetForce()
-//Retn: integer amount of Player's force mana points
-static int JPLua_Player_GetForce( lua_State *L ) {
-	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
-	lua_pushinteger( L, level.clients[player->clientNum].ps.fd.forcePower );
-	return 1;
-}
-
-//Func: Player:GetWeapon()
-//Retn: integer index of the weapon the Player has selected (See: 'Weapons' table)
-static int JPLua_Player_GetWeapon( lua_State *L ) {
-	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
-	lua_pushinteger( L, level.clients[player->clientNum].ps.weapon );
+	lua_pushstring( L, "x" ); lua_pushnumber( L, level.clients[player->clientNum].ps.viewangles.pitch ); lua_settable( L, top );
+	lua_pushstring( L, "y" ); lua_pushnumber( L, level.clients[player->clientNum].ps.viewangles.yaw); lua_settable( L, top );
+	lua_pushstring( L, "z" ); lua_pushnumber( L, level.clients[player->clientNum].ps.viewangles.roll ); lua_settable( L, top );
 	return 1;
 }
 
@@ -97,70 +64,6 @@ static int JPLua_Player_GetAmmo( lua_State *L ) {
 		weapon = lua_tointeger( L, 2 );
 
 	lua_pushinteger( L, level.clients[player->clientNum].ps.ammo[weaponData[weapon].ammoIndex] );
-	return 1;
-}
-
-//Func: Player:GetMaxAmmo([weapon])
-//Retn: integer amount of the max ammo for either the specified weapon, or the weapon the player has selected
-static int JPLua_Player_GetMaxAmmo( lua_State *L ) {
-	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
-	int weapon = level.clients[player->clientNum].ps.weapon;
-
-	if ( lua_gettop( L ) == 2 )
-		weapon = lua_tointeger( L, 2 );
-
-	lua_pushinteger( L, ammoData[weaponData[weapon].ammoIndex].max );
-	return 1;
-}
-
-//Func: Player:GetSaberStyle()
-//Retn: integer index of the saber style the Player is using (See: 'SaberStyle' table)
-static int JPLua_Player_GetSaberStyle( lua_State *L ) {
-	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
-	lua_pushinteger( L, level.clients[player->clientNum].ps.fd.saberDrawAnimLevel );
-	return 1;
-}
-
-//Func: Player:GetEFlags()
-//Retn: bit-mask of various entity flags (See: 'EFlags' table)
-static int JPLua_Player_GetEFlags( lua_State *L ) {
-	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
-	lua_pushinteger( L, level.clients[player->clientNum].ps.eFlags );
-	return 1;
-}
-
-//Func: Player:GetEFlags2()
-//Retn: bit-mask of various entity flags (See: 'EFlags2' table)
-static int JPLua_Player_GetEFlags2( lua_State *L ) {
-	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
-	lua_pushinteger( L, level.clients[player->clientNum].ps.eFlags2 );
-	return 1;
-}
-
-//Func: Player:GetDuelingPartner()
-//Retn: nil if Player is not dueling
-//		Player object of the client Player is dueling
-static int JPLua_Player_GetDuelingPartner( lua_State *L ) {
-	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
-	if ( !level.clients[player->clientNum].ps.duelInProgress )
-		lua_pushnil( L );
-	else
-		JPLua_Player_CreateRef( L, level.clients[player->clientNum].ps.duelIndex );
-
-	return 1;
-}
-
-//Func: Player:GetLocation()
-//Retn: string of the nearest target_location
-extern const char *CG_GetLocationString( const char *loc ); // cg_main.c
-static int JPLua_Player_GetLocation( lua_State *L ) {
-	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
-	char location[64];
-	if ( Team_GetLocationMsg( &g_entities[player->clientNum], location, sizeof( location ) ) )
-		lua_pushstring( L, location );
-	else
-		lua_pushnil( L );
-
 	return 1;
 }
 
@@ -180,6 +83,102 @@ static int JPLua_Player_GetAnimations( lua_State *L ) {
 	return 1;
 }
 
+//Func: Player:GetArmor()
+//Retn: integer amount of the Player's armor
+static int JPLua_Player_GetArmor( lua_State *L ) {
+	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
+	lua_pushinteger( L, level.clients[player->clientNum].ps.stats[STAT_ARMOR] );
+	return 1;
+}
+
+//Func: Player:GetDuelingPartner()
+//Retn: nil if Player is not dueling
+//		Player object of the client Player is dueling
+static int JPLua_Player_GetDuelingPartner( lua_State *L ) {
+	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
+	if ( !level.clients[player->clientNum].ps.duelInProgress )
+		lua_pushnil( L );
+	else
+		JPLua_Player_CreateRef( L, level.clients[player->clientNum].ps.duelIndex );
+
+	return 1;
+}
+
+//Func: Player:GetEFlags()
+//Retn: bit-mask of various entity flags (See: 'EFlags' table)
+static int JPLua_Player_GetEFlags( lua_State *L ) {
+	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
+	lua_pushinteger( L, level.clients[player->clientNum].ps.eFlags );
+	return 1;
+}
+
+//Func: Player:GetEFlags2()
+//Retn: bit-mask of various entity flags (See: 'EFlags2' table)
+static int JPLua_Player_GetEFlags2( lua_State *L ) {
+	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
+	lua_pushinteger( L, level.clients[player->clientNum].ps.eFlags2 );
+	return 1;
+}
+
+//Func: Player:GetForce()
+//Retn: integer amount of Player's force mana points
+static int JPLua_Player_GetForce( lua_State *L ) {
+	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
+	lua_pushinteger( L, level.clients[player->clientNum].ps.fd.forcePower );
+	return 1;
+}
+
+//Func: Player:GetHealth()
+//Retn: integer amount of the Player's health
+static int JPLua_Player_GetHealth( lua_State *L ) {
+	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
+	lua_pushinteger( L, level.clients[player->clientNum].ps.stats[STAT_HEALTH] );
+	return 1;
+}
+
+//Func: Player:GetID()
+//Retn: integer of the client's ID
+static int JPLua_Player_GetID( lua_State *L ) {
+	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
+	lua_pushinteger( L, player->clientNum );
+	return 1;
+}
+
+//Func: Player:GetLocation()
+//Retn: string of the nearest target_location
+extern const char *CG_GetLocationString( const char *loc ); // cg_main.c
+static int JPLua_Player_GetLocation( lua_State *L ) {
+	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
+	char location[64];
+	if ( Team_GetLocationMsg( &g_entities[player->clientNum], location, sizeof( location ) ) )
+		lua_pushstring( L, location );
+	else
+		lua_pushnil( L );
+
+	return 1;
+}
+
+//Func: Player:GetMaxAmmo([weapon])
+//Retn: integer amount of the max ammo for either the specified weapon, or the weapon the player has selected
+static int JPLua_Player_GetMaxAmmo( lua_State *L ) {
+	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
+	int weapon = level.clients[player->clientNum].ps.weapon;
+
+	if ( lua_gettop( L ) == 2 )
+		weapon = lua_tointeger( L, 2 );
+
+	lua_pushinteger( L, ammoData[weaponData[weapon].ammoIndex].max );
+	return 1;
+}
+
+//Func: Player:GetName()
+//Retn: string of the Player's name
+static int JPLua_Player_GetName( lua_State *L ) {
+	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
+	lua_pushstring( L, level.clients[player->clientNum].pers.netname );
+	return 1;
+}
+
 //Func: Player:GetPosition()
 //Retn: Table of x/y/z position
 static int JPLua_Player_GetPosition( lua_State *L ) {
@@ -195,18 +194,37 @@ static int JPLua_Player_GetPosition( lua_State *L ) {
 	return 1;
 }
 
-//Func: Player:GetAngles()
-//Retn: Table of pitch/yaw/roll view angles
-static int JPLua_Player_GetAngles( lua_State *L ) {
-	int top = 0;
+//Func: Player:GetSaberStyle()
+//Retn: integer index of the saber style the Player is using (See: 'SaberStyle' table)
+static int JPLua_Player_GetSaberStyle( lua_State *L ) {
 	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
+	lua_pushinteger( L, level.clients[player->clientNum].ps.fd.saberDrawAnimLevel );
+	return 1;
+}
+
+//Func: Player:GetUserinfo()
+//Retn: Table of userinfo keys/values
+static int JPLua_Player_GetUserinfo( lua_State *L ) {
+	const char *s;
+	char userinfo[MAX_INFO_STRING], key[BIG_INFO_KEY], value[BIG_INFO_VALUE];
+	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
+	int top = 0;
 
 	lua_newtable( L );
 	top = lua_gettop( L );
 
-	lua_pushstring( L, "x" ); lua_pushnumber( L, level.clients[player->clientNum].ps.viewangles.pitch ); lua_settable( L, top );
-	lua_pushstring( L, "y" ); lua_pushnumber( L, level.clients[player->clientNum].ps.viewangles.yaw); lua_settable( L, top );
-	lua_pushstring( L, "z" ); lua_pushnumber( L, level.clients[player->clientNum].ps.viewangles.roll ); lua_settable( L, top );
+	//RAZTODO: cache userinfo somehow :/
+	trap->GetUserinfo( player->clientNum, userinfo, sizeof( userinfo ) );
+	s = userinfo;
+	while ( s ) {
+		Info_NextPair( &s, key, value );
+
+		if ( !key[0] )
+			break;
+
+		lua_pushstring( L, key ); lua_pushstring( L, value ); lua_settable( L, top );
+	}
+
 	return 1;
 }
 
@@ -225,14 +243,11 @@ static int JPLua_Player_GetVelocity( lua_State *L ) {
 	return 1;
 }
 
-//Func: Player:IsWeaponHolstered()
-//Retn: integer expressing the holstered state of the saber
-//		0 - all applicable sabers are activated
-//		1 - using dual/staff and only one blade is activated
-//		2 - both blades are off
-static int JPLua_Player_IsWeaponHolstered( lua_State *L ) {
+//Func: Player:GetWeapon()
+//Retn: integer index of the weapon the Player has selected (See: 'Weapons' table)
+static int JPLua_Player_GetWeapon( lua_State *L ) {
 	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
-	lua_pushinteger( L, g_entities[player->clientNum].s.saberHolstered );
+	lua_pushinteger( L, level.clients[player->clientNum].ps.weapon );
 	return 1;
 }
 
@@ -256,6 +271,17 @@ static int JPLua_Player_IsAlive( lua_State *L ) {
 static int JPLua_Player_IsBot( lua_State *L ) {
 	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
 	lua_pushboolean( L, !!(g_entities[player->clientNum].r.svFlags & SVF_BOT) );
+	return 1;
+}
+
+//Func: Player:IsWeaponHolstered()
+//Retn: integer expressing the holstered state of the saber
+//		0 - all applicable sabers are activated
+//		1 - using dual/staff and only one blade is activated
+//		2 - both blades are off
+static int JPLua_Player_IsWeaponHolstered( lua_State *L ) {
+	jplua_player_t *player = JPLua_CheckPlayer( L, 1 );
+	lua_pushinteger( L, g_entities[player->clientNum].s.saberHolstered );
 	return 1;
 }
 
@@ -285,26 +311,27 @@ jplua_player_t *JPLua_CheckPlayer( lua_State *L, int idx ) {
 
 static const struct luaL_Reg jplua_player_meta[] = {
 	{ "__tostring",			JPLua_Player_ToString },
-	{ "GetID",				JPLua_Player_GetID },
-	{ "GetName",			JPLua_Player_GetName },
-	{ "GetHealth",			JPLua_Player_GetHealth },
-	{ "GetArmor",			JPLua_Player_GetArmor },
-	{ "GetForce",			JPLua_Player_GetForce },
-	{ "GetWeapon",			JPLua_Player_GetWeapon },
 	{ "GetAmmo",			JPLua_Player_GetAmmo },
-	{ "GetMaxAmmo",			JPLua_Player_GetMaxAmmo },
-	{ "GetSaberStyle",		JPLua_Player_GetSaberStyle },
+	{ "GetAngles",			JPLua_Player_GetAngles },
+	{ "GetAnimations",		JPLua_Player_GetAnimations },
+	{ "GetArmor",			JPLua_Player_GetArmor },
+	{ "GetDuelingPartner",	JPLua_Player_GetDuelingPartner },
 	{ "GetEFlags",			JPLua_Player_GetEFlags },
 	{ "GetEFlags2",			JPLua_Player_GetEFlags2 },
-	{ "GetDuelingPartner",	JPLua_Player_GetDuelingPartner },
+	{ "GetForce",			JPLua_Player_GetForce },
+	{ "GetHealth",			JPLua_Player_GetHealth },
+	{ "GetID",				JPLua_Player_GetID },
 	{ "GetLocation",		JPLua_Player_GetLocation },
-	{ "GetAnimations",		JPLua_Player_GetAnimations },
+	{ "GetMaxAmmo",			JPLua_Player_GetMaxAmmo },
+	{ "GetName",			JPLua_Player_GetName },
 	{ "GetPosition",		JPLua_Player_GetPosition },
-	{ "GetAngles",			JPLua_Player_GetAngles },
+	{ "GetSaberStyle",		JPLua_Player_GetSaberStyle },
+	{ "GetUserinfo",		JPLua_Player_GetUserinfo },
 	{ "GetVelocity",		JPLua_Player_GetVelocity },
-	{ "IsWeaponHolstered",	JPLua_Player_IsWeaponHolstered },
+	{ "GetWeapon",			JPLua_Player_GetWeapon },
 	{ "IsAlive",			JPLua_Player_IsAlive },
 	{ "IsBot",				JPLua_Player_IsBot },
+	{ "IsWeaponHolstered",	JPLua_Player_IsWeaponHolstered },
 
 	{ NULL, NULL }
 };
