b15b8cc8112437b6bad970331de930ddd69252ad
EpicLoyd <boriscool007@gmail.com>
2015-07-25 18:38:03 +1000

    Added some checks for jplua weapon

--- game/g_luaweapon.cpp
+++ game/g_luaweapon.cpp
@@ -11,14 +11,14 @@ qboolean JPLua_Weapon_CallFunction(gentity_t *ent,weapon_t type, qboolean altFir
 #ifdef JPLUA
 	lua_weapon *handle = &weapon_func_list[type];
 	if (altFire){
-		if (handle->altFire != 0) {
+		if (handle->altFire != 0 || handle->altFire != -1) {
 			lua_rawgeti(JPLua.state, LUA_REGISTRYINDEX, weapon_func_list[type].altFire);
 			JPLua_Entity_CreateRef(JPLua.state, ent);
 			JPLua_Call(JPLua.state, 1, 0);
 			return qtrue;
 		}
 	}
-	if (handle->fire != 0) {
+	if (handle->fire != 0 || handle->fire != -1) {
 		lua_rawgeti(JPLua.state, LUA_REGISTRYINDEX, weapon_func_list[type].fire);
 		JPLua_Entity_CreateRef(JPLua.state, ent);
 		JPLua_Call(JPLua.state, 1, 0);
@@ -31,6 +31,9 @@ qboolean JPLua_Weapon_CallFunction(gentity_t *ent,weapon_t type, qboolean altFir
 int JPLua_Weapon_SetFireFunction(lua_State *L){
 	weapon_t type = (weapon_t)luaL_checkinteger(L, 1);
 	lua_weapon *handle = &weapon_func_list[type];
+	if (lua_type(L, 2) != LUA_TFUNCTION){
+		return 0;
+	}
 	if (handle != nullptr) {
 		handle->fire = luaL_ref(L, LUA_REGISTRYINDEX);
 	}
@@ -45,6 +48,9 @@ int JPLua_Weapon_SetFireFunction(lua_State *L){
 int JPLua_Weapon_SetAltFireFunction(lua_State *L){
 	weapon_t type = (weapon_t)luaL_checkinteger(L, 1);
 	lua_weapon *handle = &weapon_func_list[type];
+	if (lua_type(L, 2) != LUA_TFUNCTION){
+		return 0;
+	}
 	if (handle != nullptr) {
 		handle->altFire = luaL_ref(L, LUA_REGISTRYINDEX);
 	}
