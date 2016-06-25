453f7582a2a5dca8662ff373a46b1ae817591d93
Razish <mrrazish@gmail.com>
2016-06-26 03:01:57 +1000

    Improve viewmodel sway

--- cgame/cg_weapons.cpp
+++ cgame/cg_weapons.cpp
@@ -181,17 +181,16 @@ static void CG_CalculateWeaponPosition( vector3 *origin, vector3 *angles ) {
 	if ( cg_gunMomentumEnable.integer ) {
 		// sway viewmodel when changing viewangles
 		static vector3 previousAngles{};
-		static int previousTime = 0;
 
 		vector3 deltaAngles;
 		AnglesSubtract( angles, &previousAngles, &deltaAngles );
 		VectorScale( &deltaAngles, 1.0f, &deltaAngles );
 
-		const double dampTime = (double)(cg.time - previousTime) * (1.0 / (double)cg_gunMomentumInterval.integer);
-		const double dampRatio = std::pow( std::abs( (double)cg_gunMomentumDamp.value ), dampTime );
+		const double f = std::abs( 1.0 / (double)cg_gunMomentumDamp.value );
+		const double y = (double)cg_gunMomentumInterval.value;
+		const double dampRatio = 1.0 / std::pow( f, y );
 		VectorMA( &previousAngles, dampRatio, &deltaAngles, angles );
 		VectorCopy( angles, &previousAngles );
-		previousTime = cg.time;
 
 		// move viewmodel downwards when jumping etc
 		static float previousOriginZ = 0.0f;
