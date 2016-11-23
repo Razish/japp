b45c240bc147e815217cd266683e8b981b8e0a97
Razish <mrrazish@gmail.com>
2016-11-23 19:06:08 +1100

    Fixed build/clean scripts for Windows

--- clean.bat
+++ clean.bat
@@ -2,5 +2,4 @@
 
 for %%X in (scons.bat, lua.exe) do (IF EXIST %%~$PATH:X echo Found %%X at %%~$PATH:X)
 
-scons -Q debug=0 force32=0 no_sql=1 tools=default
-@rem lua package.lua
+scons -Qc debug=0 force32=0 no_sql=1 tools=default
